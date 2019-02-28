dea67abd48c45bff991bd2b3611aec7279f72a82
Victor Ivanov <justicejesus1237@gmail.com>
2019-02-28 22:49:44 +0300

    усложненное ДЗ

---
+++ script.js
@@ -0,0 +1,6 @@
+const num = 33721;
+const amount = [3, 3, 7, 2, 1];
+const result = (amount[0] * amount[1] * amount[2] * amount[3] * amount[4]);
+console.log(result);
+console.log(result ** 3);
+alert("20")
